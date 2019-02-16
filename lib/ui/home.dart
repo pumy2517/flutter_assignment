73d78a585dc791b835ff512d5a37db6098b029e5
pumy2517 <60070067@kmitl.ac.th>
2019-02-16 21:31:11 +0700

    Add bottom nav bar

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -9,103 +9,103 @@ class MainScreen extends StatefulWidget {
 
 class MainScreenState extends State<StatefulWidget> {
   @override
-  int _currentIndex = 0;
-  Widget showText(int currentIndex) {
-    switch (currentIndex) {
-      case 0:
-        return Text(
-          "Home",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 40, color: Colors.grey),
-        );
-      case 1:
-        return Text(
-          "Notify",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 40, color: Colors.grey),
-        );
-      case 2:
-        return Text(
-          "Map",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 40, color: Colors.grey),
-        );
-      case 3:
-        return Text(
-          "Profile",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 40, color: Colors.grey),
-        );
-      case 4:
-        return Text(
-          "Setup",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, fontSize: 40, color: Colors.grey),
-        );
-        break;
-      default:
-        return Text("Home");
-    }
-  }
-
   Widget build(BuildContext context) {
     return MaterialApp(
       home: DefaultTabController(
-        length: 10,
+        length: 5,
         child: Scaffold(
           appBar: AppBar(
             centerTitle: true,
             title: Text("Home"),
           ),
-          body: Center(child: (showText(_currentIndex))),
-          bottomNavigationBar: Theme(
-            data: Theme.of(context).copyWith(
-                canvasColor: Colors.blue,
-                primaryColor: Colors.white,
-                textTheme: Theme.of(context).textTheme.copyWith(
-                    caption:
-                        new TextStyle(color: Colors.lightBlueAccent[200]))),
-            child: BottomNavigationBar(
-              type: BottomNavigationBarType.fixed,
-              currentIndex: _currentIndex,
-              onTap: (value) {
-                _currentIndex = value;
-                setState(() {});
-              },
-              items: [
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.list),
-                    title: Text(
-                      "",
-                      style: TextStyle(height: 0.0),
-                    )),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.notifications),
-                    title: Text(
-                      "",
-                      style: TextStyle(height: 0.0),
-                    )),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.explore),
-                    title: Text(
-                      "",
-                      style: TextStyle(height: 0.0),
-                    )),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.person),
-                    title: Text(
-                      "",
-                      style: TextStyle(height: 0.0),
-                    )),
-                BottomNavigationBarItem(
-                    icon: Icon(Icons.settings),
-                    title: Text(
-                      "",
-                      style: TextStyle(height: 0.0),
-                    )),
-              ],
-            ),
+          body: TabBarView(
+            children: <Widget>[
+              Container(
+                child: Center(
+                  child: Text(
+                    "Home",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.grey),
+                  ),
+                ),
+                color: Colors.white,
+              ),
+              Container(
+                child: Center(
+                  child: Text(
+                    "Notify",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.grey),
+                  ),
+                ),
+                color: Colors.white,
+              ),
+              Container(
+                child: Center(
+                  child: Text(
+                    "Map",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.grey),
+                  ),
+                ),
+                color: Colors.white,
+              ),
+              Container(
+                child: Center(
+                  child: Text(
+                    "Profile",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.grey),
+                  ),
+                ),
+                color: Colors.white,
+              ),
+              Container(
+                child: Center(
+                  child: Text(
+                    "Setup",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 40,
+                        color: Colors.grey),
+                  ),
+                ),
+                color: Colors.white,
+              ),
+            ],
           ),
+          bottomNavigationBar: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.view_compact),
+              ),
+              Tab(
+                icon: Icon(Icons.notifications),
+              ),
+              Tab(
+                icon: Icon(Icons.explore),
+              ),
+              Tab(
+                icon: Icon(Icons.person),
+              ),
+              Tab(
+                icon: Icon(Icons.settings),
+              ),
+            ],
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white24,
+            indicatorSize: TabBarIndicatorSize.label,
+            indicatorColor: Colors.white,
+          ),
+          backgroundColor: Colors.blue,
         ),
       ),
     );
